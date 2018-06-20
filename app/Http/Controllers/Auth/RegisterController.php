f85df296e84cf09906c88d851909990a9c1fd4ee
Christian G Fritsch <frontend.guilherme@gmail.com>
2018-06-20 09:02:25 -0300

    Iniciando os testes para implementar a confirmação de email

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
+use Jrean\UserVerification\Traits\VerifiesUsers;
 
 class RegisterController extends Controller
 {
@@ -22,6 +23,7 @@ class RegisterController extends Controller
     */
 
     use RegistersUsers;
+    use VerifiesUsers;
 
     /**
      * Where to redirect users after registration.
@@ -68,5 +70,99 @@ class RegisterController extends Controller
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
+    }
+
+    /**
+     * Handle a registration request for the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function register(Request $request)
+    {
+        $summoner = Summoner::where('name', $request->name)->first();
+        if ($summoner) {
+            return response([
+                'errors' => [
+                    [
+                        'details' => 'Nome de invocador já cadastrado!'
+                    ]
+                ]
+            ], 400);
+        }
+
+        $region = Region::where('id', $request->region_id)->first();
+        $client = new Client([
+            'base_uri' => "https://" . $region->riot_id . ".api.riotgames.com/lol/",
+            'query' => [
+                'api_key' => env('RIOT_API_KEY')
+            ]
+        ]);
+        try {
+            $summonerResponse = $client->get("summoner/v3/summoners/by-name/$request->nickname");
+        } catch (ClientException $exception) {
+            return response([
+                'errors' => [
+                    [
+                        'status' => 404,
+                        'detail' => 'Nome de invocador não encontrado!'
+                    ]
+                ],
+            ], 400);
+        }
+        $riot_summoner = json_decode($summonerResponse->getBody());
+        $rankingResponse = $client->get("league/v3/positions/by-summoner/$riot_summoner->id");
+        $rankings = json_decode($rankingResponse->getBody());
+
+        if (sizeof($rankings) == 0) {
+            $ranking = (object) [
+                'tier' => 1
+            ];
+
+        } else {
+            $ranking = $rankings[0];
+            foreach ($rankings as $r) {
+                if ($this->tierToInt($r->tier) > $this->tierToInt($ranking->tier)) {
+                    $ranking->tier = $r->id;
+                }
+            }
+        }
+
+        $tier = Tier::where('riot_id', $ranking->tier)->first();
+
+        $summoner = new Summoner;
+        $summoner->id = $riot_summoner->id;
+        $summoner->profile_icon_id = $riot_summoner->profileIconId;
+        $summoner->name = $riot_summoner->name;
+        $summoner->level = $riot_summoner->summonerLevel;
+        $summoner->revision_date = $riot_summoner->revisionDate;
+        $summoner->account_id = $riot_summoner->accountId;
+        $summoner->tier_id = $tier['id'] || 1;
+        $summoner->save();
+
+        $user = User::create([
+            'email' => $request->email,
+            'nickname' => $request->nickname,
+            'region_id' => $request->region_id,
+            'password' => bcrypt($request->password),
+            'summoner_id' => $riot_summoner->id
+        ]);
+
+//        $this->validator($request->all())->validate();
+//
+//        $user = $this->create($request->all());
+
+        event(new Registered($user));
+
+        $this->guard()->login($user);
+
+        UserVerification::generate($user);
+
+        UserVerification::send($user, 'My Custom E-mail Subject');
+
+
+
+        return $this->registered($request, $user)
+            ?: redirect($this->redirectPath());
     }
 }
--- config/app.php
+++ config/app.php
@@ -150,7 +150,7 @@ return [
         /*
          * Package Service Providers...
          */
-
+        Jrean\UserVerification\UserVerificationServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -205,6 +205,7 @@ return [
         'Schema' => Illuminate\Support\Facades\Schema::class,
         'Session' => Illuminate\Support\Facades\Session::class,
         'Storage' => Illuminate\Support\Facades\Storage::class,
+        'UserVerification' => Jrean\UserVerification\Facades\UserVerification::class,
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
